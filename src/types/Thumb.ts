db51dbcb4e364fd1644043df14ba510f5a31ed5d
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2023-03-28 22:25:58 -0300

    🏷️  types: cria tipo de thumb do cloudinary

---
+++ src/types/Thumb.ts
@@ -0,0 +1,20 @@
+export interface eventProps {
+  info: {
+    public_id: string;
+    version: number;
+    signature: string;
+    width: number;
+    height: number;
+    format: string;
+    resource_type: string;
+    created_at: string;
+    tags: [];
+    bytes: number;
+    type: string;
+    etag: string;
+    placeholder: boolean;
+    url: string;
+    secure_url: string;
+    original_filename: string;
+  };
+}
