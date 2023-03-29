fd9a046f0fc3c01fc20e15dc80053edc0acd6250
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2023-03-28 23:11:17 -0300

    ♻️ refactor: adiciona hydation
    Co-authored-by: Ikarosv <ikarovieiraa@gmail.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,37 @@
-import type { AppProps } from 'next/app'
-import { SessionProvider } from "next-auth/react"
-import '../styles/globals.sass'
+import type { AppProps } from 'next/app';
+import { useRef, ReactNode } from 'react';
+import { SessionProvider } from 'next-auth/react';
+import { QueryClient, QueryClientProvider, Hydrate, DehydratedState } from 'react-query';
+import '../styles/globals.sass';
+import '../styles/ck.css';
+import { Session } from 'next-auth';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { Provider } from 'jotai/react';
+import { useHydrateAtoms } from 'jotai/utils';
+import { queryClientAtom } from 'jotai-tanstack-query';
 
-export default function App({Component, pageProps: { session, ...pageProps }}: AppProps) {
+
+export default function App({Component, pageProps: { session, dehydratedState, ...pageProps }}: AppProps) {
+  const queryClient = useRef(new QueryClient());
+
+  const HydrateAtoms = ({ children }: {children: ReactNode}) => {
+    useHydrateAtoms([[queryClientAtom, queryClient.current]]);
+    return children as JSX.Element;
+  };
+  
+
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
+    <SessionProvider session={session as Session}>
+      <QueryClientProvider client={queryClient.current}>
+        <Provider>
+          <Hydrate state={dehydratedState as DehydratedState} >
+            <HydrateAtoms>
+              <Component {...pageProps} />
+              <ReactQueryDevtools initialIsOpen={false} />
+            </HydrateAtoms>
+          </Hydrate>
+        </Provider>
+      </QueryClientProvider>
     </SessionProvider>
-  )
+  );
 }
