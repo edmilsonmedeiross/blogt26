1e0f1f1b5a4458da4e7fae0a5b1eaea0394ab374
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2023-03-28 23:54:01 -0300

    ♻️ refactor
    Co-authored-by: Ikarosv <ikarovieiraa@gmail.com>

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,24 +1,25 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import searchUser from "../../services/searchUser";
+import { NextApiRequest, NextApiResponse } from 'next';
+import searchUser from '../../services/searchUser';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    return res.status(400).json({ error: "Missing email and/or password" });
+    return res.status(400).json({ error: 'Missing email and/or password' });
   }
 
-  const data = { email, password }
-  
+  const data = { email, password };
+
   const verify: any = await searchUser(data);
   // console.log('verify', verify);
-  
 
-
-  if(verify) {
+  if (verify) {
     res.status(200).json({ data: verify });
     return;
   }
 
-  res.status(400).json({ error: "Invalid email and/or password" });
+  res.status(400).json({ error: 'Invalid email and/or password' });
 }
