e1dd205371198570ff6a5603d68799ee4ea14a2e
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2023-03-29 17:59:50 -0300

    ♻️  refactor: refatora atoms
    Co-authored-by: Ikarosv <ikarovieiraa@gmail.com>

--- src/jotai/JotaiCategories.ts
+++ src/jotai/JotaiCategories.ts
@@ -1,6 +1,7 @@
 import { TypeCategories, DBCategories } from '../types/Categories';
 import axios from 'axios';
 import { atomsWithQuery } from 'jotai-tanstack-query';
+import { atomWithStorage, loadable } from 'jotai/utils';
 
 interface ApiResponse {
   categories: DBCategories[];
@@ -9,12 +10,15 @@ interface ApiResponse {
 export const fetchCategories = async (): Promise<TypeCategories[]> => {
   const {data} = await axios.get<ApiResponse>('http://localhost:3000/api/posts/categories');
   const categories = data
-    .categories.map<TypeCategories>((categoria: DBCategories) => ({...categoria, checked: false}));
+    .categories.map<TypeCategories>((category: DBCategories) => ({...category, checked: false}));
   return categories;
 };
 
 export const [atomCategories] = atomsWithQuery(() => ({
   queryKey: ['categories'],
-  queryFn: fetchCategories
+  queryFn: fetchCategories,  
 }));
 
+export const LoadableAtomCategories = loadable(atomCategories);
+
+export const AtomCat = atomWithStorage<TypeCategories[]>('savedCategories', []);
