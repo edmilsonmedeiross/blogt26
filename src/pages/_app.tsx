495cce596bdcd3a3088f333272bbd224cc2da112
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2023-03-31 16:19:58 -0300

    ♻️ Refactor(tinyMCE):
    refatora codigo para uso do tinyMCE

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,16 @@ import type { AppProps } from 'next/app';
 import { useRef, ReactNode } from 'react';
 import { SessionProvider } from 'next-auth/react';
 import { QueryClient, QueryClientProvider, Hydrate, DehydratedState } from 'react-query';
-import '../styles/globals.sass';
-import '../styles/ck.css';
 import { Session } from 'next-auth';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { Provider } from 'jotai/react';
 import { useHydrateAtoms } from 'jotai/utils';
 import { queryClientAtom } from 'jotai-tanstack-query';
+// import 'react-quill/dist/quill.snow.css';
+import '../tinymce/js/tinymce/skins/ui/oxide/skin.min.css';
+// import '../tinymce/js/tinymce/skins/ui/oxide-dark/skin.min.css';
+import '../styles/globals.sass';
+// import '../styles/ck.css';
 
 
 export default function App({Component, pageProps: { session, dehydratedState, ...pageProps }}: AppProps) {
